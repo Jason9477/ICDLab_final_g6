643a115ced0e948274b278c857d7ca8f228a3b8c
Jason Hsieh <jasonhsieh@Jasons-MacBook-Pro.local>
2026-05-19 03:12:58 +0800

    0519

--- 00_TB/tb.v
+++ 00_TB/tb.v
@@ -8,21 +8,25 @@ module tb;
     reg rst_n;
     reg [7:0] a;
     reg [7:0] b;
-    wire [15:0] c;
-    reg [15:0] data_mem [0:97];
+    wire [11:0] Vout;
+    reg [7:0] a_mem [0:97];
+    reg [7:0] b_mem [0:97];
     reg in_en;
+    wire valid;
     LK uut (
         .clk(clk),
         .rst_n(rst_n),
         .a(a),
         .b(b),
-        .in_en(in_en)
+        .in_en(in_en),
+        .valid(valid),
+        .Vout(Vout)
     );
     initial begin
-        //$dumpfile("LK.vcd");
-        //$dumpvars();
-         $fsdbDumpfile("LK.fsdb");
-         $fsdbDumpvars("+mda", tb, uut);
+        $dumpfile("LK.vcd");
+        $dumpvars();
+        // $fsdbDumpfile("LK.fsdb");
+        // $fsdbDumpvars("+mda", tb, uut);
 
     end
     initial begin
@@ -31,16 +35,22 @@ module tb;
         b=0;
     end
     always #`HCYCLE clk = ~clk; // Clock generation
+        always @(posedge clk) begin
+        if (valid)
+            $display("valid=1  Vout = %h", Vout);
+    end
 integer i;
     initial begin
-        $readmemh("../00_TB/input_combined.txt", data_mem);
+        $readmemh("../00_TB/a.txt", a_mem);
+        $readmemh("../00_TB/b.txt", b_mem);
         rst_n = 0; // Reset active
 
         #`CYCLE; // Wait for reset to be released
         in_en = 1;
         rst_n = 1; // Release reset
         for (i = 0; i < 98; i = i + 1) begin
-                {a, b} = data_mem[i];
+                a = a_mem[i];
+                b = b_mem[i];
                 #`CYCLE;   // 一個 cycle
             end
 
